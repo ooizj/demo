63442abf4bc7ffeeeaccb007c10cf38b6d16ff56
jun.zhao <N@00000000>
2020-03-03 10:37:15 +0800

    tmp commit

--- testkafka210/src/test/java/me/ooi/demo/testkafka210/TestConsumer.java
+++ testkafka210/src/test/java/me/ooi/demo/testkafka210/TestConsumer.java
@@ -3,11 +3,14 @@ package me.ooi.demo.testkafka210;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 
 /**
  * @author jun.zhao
@@ -21,6 +24,40 @@ public class TestConsumer {
 	    props.load(TestConsumer.class.getResourceAsStream("/consumer.properties"));
 	    KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(props);
 	    consumer.subscribe(Arrays.asList(TOPIC));
+	    
+	    Runtime.getRuntime().addShutdownHook(new Thread(()->{
+	        consumer.close();
+	    }));
+
+	    //轮询消息
+	    while (true) {
+	        ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofSeconds(10)); //最多等10s 
+	        if( records != null ){
+	            System.out.println("本次获取到的消息数量："+records.count());
+	            for (ConsumerRecord<Integer, String> record : records) {
+	            	System.out.println("收到消息："+record.value()) ; 
+	            }
+	        }
+	    }
+	}
+	
+	public void testSeek() throws IOException {
+	    final String TOPIC = "topic-9" ; 
+	    Properties props = new Properties() ; 
+	    props.load(TestConsumer.class.getResourceAsStream("/consumer.properties"));
+	    KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(props);
+	    consumer.subscribe(Arrays.asList(TOPIC), new ConsumerRebalanceListener() {
+			@Override
+			public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+			}
+			@Override
+			public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+				//设置offset为0以获取包含以前的所有数据
+//				consumer.seek(new TopicPartition(TOPIC, 0), 0);
+				consumer.seekToBeginning(partitions) ; 
+			}
+		});
+	    
 
 	    Runtime.getRuntime().addShutdownHook(new Thread(()->{
 	        consumer.close();
@@ -39,7 +76,8 @@ public class TestConsumer {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		new TestConsumer().run();
+//		new TestConsumer().run();
+		new TestConsumer().testSeek();
 	}
 
 }
