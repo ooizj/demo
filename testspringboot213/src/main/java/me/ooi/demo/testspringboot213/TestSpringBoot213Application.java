7ac3e4fcc4e77e57c85abb43ab8e8549b49e52d0
zhaojun <zhaojun@expservice.com.cn>
2020-06-18 20:13:12 +0800

    add testspringboot213

---
+++ testspringboot213/src/main/java/me/ooi/demo/testspringboot213/TestSpringBoot213Application.java
@@ -0,0 +1,19 @@
+package me.ooi.demo.testspringboot213;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author jun.zhao
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableWebMvc
+public class TestSpringBoot213Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBoot213Application.class, args);
+	}
+	
+}
